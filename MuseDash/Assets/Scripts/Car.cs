cd89c12351c7f24073528811502171e4c4355ce1
KID <50666207+KID421@users.noreply.github.com>
2021-05-05 20:05:43 +0800

    認識方法、事件與方法的參數

--- MuseDash/Assets/Scripts/Car.cs
+++ MuseDash/Assets/Scripts/Car.cs
@@ -2,6 +2,7 @@
 
 public class Car : MonoBehaviour
 {
+    #region 欄位
     // 單行註解 KID 2021.04.28
 
     /*
@@ -66,4 +67,79 @@ public class Car : MonoBehaviour
     public Transform tra;
     public Camera cam;
     public AudioListener aud;
+    #endregion
+
+    #region 方法
+    // 欄位語法
+    // 修飾詞 類型 名稱 指定 值；
+
+    // 方法 Method - 儲存程式區塊或演算法
+    // 語法：
+    // 修飾詞 傳回類型 名稱 ( 參數 ) { 程式區塊或演算法 }
+    // void 無傳回 - 沒有傳回資料
+    // 方法必須被呼叫才會執行
+    /// <summary>
+    /// KID 05.05
+    /// 這是一個會輸出訊息的測試方法
+    /// </summary>
+    private void Test()
+    {
+        // 輸出(任何資料)；
+        print("我是測試方法");
+    }
+    // 速度 50、100、150
+    // 加上音效
+    // 維護性
+    private void Drive50()
+    {
+        print("開車速度：" + 50);
+        print("音效");
+    }
+    private void Drive100()
+    {
+        print("開車速度：" + 100);
+        print("音效");
+    }
+    private void Drive150()
+    {
+        print("開車速度：" + 150);
+        print("音效");
+    }
+
+    // 參數 Paramater 語法：類型 名稱 指定 預設值
+    // 有預設值的參數必須寫在最右邊 (選填式參數)
+    // 呼叫的時候可以決定參數的值
+    // 類型 名稱，類型 名稱，....
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="speed">速度</param>
+    /// <param name="sound">音效</param>
+    /// <param name="effect">特效</param>
+    private void Drive(int speed, string sound = "ㄎㄎㄎ", string effect = "雜質")
+    {
+        print("開車速度：" + speed);
+        print("音效：" + sound);
+        print("特效：" + effect);
+    }
+    #endregion
+
+    #region 事件
+    // 事件 Event - 在特定時間點會已指定次數執行 (程式的入口)
+    // 開始事件：播放遊戲後會執行一次
+    // 應用：初始值，500 塊初始金錢、三條生命等等..
+    private void Start()
+    {
+        // 呼叫方法
+        // 方法名稱()；
+        Test();
+        Drive50();
+        // 有幾個參數就要輸入幾個
+        Drive(99, "咻咻咻", "灰塵");
+        Drive(777, "轟轟轟", "爆炸");
+
+        Drive(10);
+        Drive(20, "嘿嘿嘿");
+    }
+    #endregion
 }
