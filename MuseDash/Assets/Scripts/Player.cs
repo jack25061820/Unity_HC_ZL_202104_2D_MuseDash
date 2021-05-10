4b52fa8a546f163f5af0ab161a2b9d5a1e64a6d8
KID <50666207+KID421@users.noreply.github.com>
2021-05-10 18:58:29 +0800

    練習定義玩家的方法

--- MuseDash/Assets/Scripts/Player.cs
+++ MuseDash/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 
 public class Player : MonoBehaviour
 {
+    #region 欄位
     [Header("跳躍高度"), Range(0, 3000)]
     public int jump = 100;
     [Header("血量"), Range(0, 2000)]
@@ -15,4 +16,49 @@ public class Player : MonoBehaviour
     private AudioSource aud;
     private Rigidbody2D rig;
     private Animator ani;
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">收到的傷害</param>
+    private void Hit(float damage)
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private bool Dead()
+    {
+        return false;
+    }
+
+    /// <summary>
+    /// 加分
+    /// </summary>
+    /// <param name="add">要加的分數</param>
+    private void AddScore(int add)
+    {
+
+    }
+    #endregion
 }
